facbb8dab16785fa7a7f1086001397651574361f
Brandon Baldemar Florián Lopez <brandonflorian42@gmail.com>
2023-04-01 09:45:30 -0600

    Agregar archivos de proyecto.

---
+++ EJERCICIOS EN CLASE/Program.cs
@@ -0,0 +1,24 @@
+﻿void matriz()
+{
+    String[] nombre = new String[]
+    {
+        "Anderson","Brandon","Yeimi","Fatima","Vivian"
+    };
+    int[,] notas = { {51,80}, { 96,76 }, { 61, 0,} };
+    int notaMasAlta = notas[0, 0];
+    String nombreNotaMasAlta = nombre[0];
+    for (int i = 0; i < notas.GetLength(0); i++)
+    {
+        for (int j = 0; j < notas.GetLength(1); j++)
+        {
+            if (notas[i, j] > notaMasAlta)
+            {
+                notaMasAlta = notas[i, j];
+                nombreNotaMasAlta = nombre[i];
+            }
+        }
+    }
+
+    Console.WriteLine("La nota más alta es: " + notaMasAlta + ", y pertenece a: " + nombreNotaMasAlta);
+}
+matriz();
